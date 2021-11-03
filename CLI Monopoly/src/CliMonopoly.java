dc693f21d4ab28f63b09a7e9139d71949c2f1dc0
TonyChoi19 <82039408+TonyChoi19@users.noreply.github.com>
2021-11-03 15:00:17 +0800

    Delete CLI Monopoly/src directory

--- CLI Monopoly/src/CliMonopoly.java
+++
@@ -1,9 +0,0 @@
-import java.net.URISyntaxException;
-
-public class CliMonopoly {
-
-    public static void main(String[] args) throws InterruptedException, URISyntaxException {
-        Controller controller = new Controller();
-        controller.start();
-    }
-}
